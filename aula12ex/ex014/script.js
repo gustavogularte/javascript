78d3169e2309f4dfba0de0b76802853586a45c4e
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2022-02-28 17:01:35 -0300

    ex012

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,13 +2,18 @@ function carregar() {
     var img = document.getElementById('foto')
     var msg = document.getElementById('msg')
     var data = new Date()
-    hora = 17
+    var hora = data.getHours()
 
-    msg.innerHTML = `Agora são ${hora}`
+    msg.innerHTML = `Agora são ${hora} horas.`
 
-    if (hora < 12) {
-        img.src = 'imagens/manha.png'
-    } else if (hora <= 18) {
-        img.scr = 'tarde.jpg'
+    if (hora >=4 && hora < 12) {
+        img.src = 'manha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        img.innerHTML = '<img src="tarde.jpg">' 
+        document.body.style.background = '#b9846f'
+    } else {
+        img.innerHTML = '<img src="noite.jpg">'
+        document.body.style.background = '#000C14'
     }
 }
