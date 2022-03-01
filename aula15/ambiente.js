ae07e5d43fde21f6fbe4f48f850fcfd279dd0c69
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2022-03-01 18:50:33 -0300

    aula15

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5, 2, 3, 7]
+
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
---
+++ aula15/vetornatela.js
@@ -0,0 +1,5 @@
+let num = [3, 5, 6, 1, 8]
+
+for (let pos in num) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
